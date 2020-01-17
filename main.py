58ad067701b3ad502dea414500cda30b7c77b297
conor <clmurphy@sfu.ca>
2020-01-17 15:10:25 -0800

    continued.

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ from shutil import copyfile
 
 def main():
 
-    # file = UserQuery.get_files("Choose an almost syn file to copy and then synify")[0]
-    file = "C:/Users/conor/PycharmProjects/FuzzyCognitaveMapDecoder/resources/syn.csv"
+    file = UserQuery.get_files("Choose an almost syn file to copy and then synify")[0]
+    # file = "C:/Users/conor/PycharmProjects/FuzzyCognitaveMapDecoder/resources/syn.csv"
     arr = file.split(".")
     temp = arr[0] + "_synify." + arr[1]
     copyfile(file, temp)
